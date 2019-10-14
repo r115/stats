5df52a19f2f688e34d6c6a83718bbaaeddbd28e1
Bernard Nandwa <bernard.nandwa@andela.com>
2019-10-14 18:58:51 +0300

    Stub out a registration route

---
+++ pages/auth/register.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>Register</div>
+</template>
+
+<script>
+export default {
+  methods: {
+    async register () {
+      const payload = {
+        name: 'Bernard Nandwa',
+        email: 'hello4@nandwa.com',
+        password: 'pass123*',
+        password_confirmation: 'pass123*'
+      }
+
+      await this.$axios.$post('/api/v1/register', payload).then(
+        function (response) {
+        // Log in the user
+        }
+      ).catch(function (error) {
+        console.log(error.response)
+      })
+    }
+  }
+}
+</script>
